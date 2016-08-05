877c40b04415fb5daa5fbe8df1410e4776fdd2c6
Timothy Savard <timsav@amazon.com>
2016-08-04 20:41:27 -0700

    Added Meme model struct.

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,20 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Savard, Tim on 8/4/16.
+//  Copyright © 2016 Savard, Tim. All rights reserved.
+//
+
+import AVKit
+
+struct Meme {
+    
+    // Meme components
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    
+    var compiledImage: UIImage
+    
+}
